1e1ae348c771eb150a620609e870127bb1196222
andreubrito <andre.ubrito@gmail.com>
2021-08-17 18:13:24 -0300

    teste

--- README.R
+++ README.R
@@ -2,8 +2,14 @@
 
 ## This is a begginer friendly script (BFC) ##
 
+# This project is a baseline for the discipline "Political Parties and Party Systems".
+# My objective is to elaborate all the tasks related to this discipline at the R environment.
+
+
 # usethis functions to directly connect the project to git and github.
 
 usethis::use_git()
 usethis::use_github()
+
+# A
 
